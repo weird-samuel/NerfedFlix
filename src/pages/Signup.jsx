67e8518604647a274c70703bc1e40b6f08369637
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-11-19 08:26:48 +0100

    started signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,12 @@
 const Signup = () => {
   return (
     <>
-      <div className="w-full"></div>
+      <div className="w-full h-screen">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/ec673412-470b-4ad5-b473-e4233cbf01a4/NG-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="///"
+        />
+      </div>
     </>
   );
 };
